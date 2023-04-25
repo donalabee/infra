bb4b2398f442cf851e837cb0f8fc8cd1b9cf7bc0
Don Alabee <donalabee2g@gmail.com>
2023-04-25 21:28:25 +0000

    Create (#16)
    * main created
    
    * providers created
    
    * infra ec2 applied
    
    * .circleci added
    
    * backend added
    
    * backend added
    
    * backend removed
    
    * destroy added
    
    * apply removed
    
    * apply removed
    
    * apply added
    
    * backend added
    
    * PR template added
    
    * terraform token added
    
    * unknown
    
    * s3 backend added

--- backend.tf
+++ backend.tf
@@ -1,9 +1,7 @@
 terraform {
-  backend "remote" {
-    organization = "KwekuDevops"
-
-    workspaces {
-      name = "infra"
-    }
+  backend "s3" {
+    bucket = "infra-tfstate-bucket"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
   }
 }
